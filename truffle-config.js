3fb3f5583707892569ba1377f93b8667e4fedb49
fbielejec <fbielejec@gmail.com>
2018-10-31 10:05:50 +0100

    init repo

---
+++ truffle-config.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = {
+  networks: {
+    local: {
+      host: 'localhost',
+      port: 8549,
+      gas: 5000000,
+      gasPrice: 5e9,
+      network_id: '*'
+    }
+  }
+};
